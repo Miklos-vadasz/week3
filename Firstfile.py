6787d95a0536c6d6c41b9de6080af728537ec3e8
Miklos Vadasz <mvadasz@stu.socsd.org>
2024-09-23 19:03:19 -0400

    did my hw

---
+++ Firstfile.py
@@ -0,0 +1,7 @@
+# Miklos Vadasz
+# September 9th 2024
+first = "Miklos"
+last = "Vadasz"
+age = "14"
+print(first + " " + last + " " + age)
+print(first, last, age)
--- hello.py
+++
@@ -1,7 +0,0 @@
-#Hello day 1 visual studio code
-print("womp")
-
-
-print("testing the other thing")
-
-print ("third time is the charm")
